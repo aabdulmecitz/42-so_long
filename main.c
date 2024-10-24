6f703320c4f23da74be1e3ecb56286bea5ac90c6
Ahmet abdulmecit Ozkaya <aozkaya@k1m05s05.42istanbul.com.tr>
2024-10-24 21:21:52 +0300

    thisone

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aozkaya <aozkaya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 03:00:21 by aabdulmecit       #+#    #+#             */
-/*   Updated: 2024/10/24 19:19:14 by aozkaya          ###   ########.fr       */
+/*   Updated: 2024/10/24 21:18:49 by aozkaya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,23 @@ int main()
 {
     void *mlx;
     void *win;
+    void *player;
 
     mlx = mlx_init();
     win = mlx_new_window(mlx, 800, 750, "My beautiful window");
 
+    mlx_new_image("Viking(Ice Age)-Sheet.png", 64, 64);
+   // mlx_put_image_to_window(mlx, win, player, 64, 64);
     mlx_mouse_show(mlx, win);
     mlx_hook(win, 17, 0, close_window, NULL);
-    int j = 0;
-    int i = 0;
-    while (j++ < 40)
-        while (i++ < 500)
-            mlx_pixel_put(mlx, win, i, i, 0xFFFFFF);
-    mlx_string_put(mlx, win, 150, 100, 0xFFFFFF, "Hi from aabdulmecitz :)");
+
+    
+    // int j = 0;
+    // int i = 0;
+    // while (j++ < 40)
+    //     while (i++ < 500)
+    //         mlx_pixel_put(mlx, win, i, i, 0xFFFFFF);
+    // mlx_string_put(mlx, win, 150, 100, 0xFFFFFF, "Hi from aabdulmecitz :)");
     mlx_loop(mlx);
     return 0;
 }
