340f9895bc0f428bd9d63563920edeab6e57a127
aabdulmecitz <ahmetabdulmecid@gmail.com>
2024-11-21 21:05:38 +0300

    commit

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: aabdulmecitz <aabdulmecitz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 03:00:21 by aabdulmecit       #+#    #+#             */
-/*   Updated: 2024/11/21 20:41:14 by aabdulmecit      ###   ########.fr       */
+/*   Updated: 2024/11/21 21:05:35 by aabdulmecit      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,11 @@
 int main(int argc, char const *argv[])
 {
     t_game *game;
+    void *image;
+    int a;
+    int b;
 
+    // a = 32;
     game = (t_game *)malloc(sizeof(t_game));
     if (!game)
         return (1);
@@ -51,14 +55,16 @@ int main(int argc, char const *argv[])
     ft_init_map(game, argv[1]);
     ft_printf("map initiated\n");
     ft_validate_map(game);
-    ft_draw_map(game);
+    image = mlx_xpm_file_to_image(game->mlx_ptr, "textures/techwall.xpm", &a, &b);
+    mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, image, 0, 0);
+    // //ft_draw_map(game);
     ft_printf("map drawn\n");
 
     // Pencere kapatma ve tuş basma olaylarını yakalama
     // mlx_hook(game->win_ptr, 33, 1L << 17, close_window, game); // 33 -> DestroyNotify için
     // mlx_key_hook(game->win_ptr, key_hook, game);
 
-    mlx_loop(game->mlx_ptr);
+    //mlx_loop(game->mlx_ptr);
     free(game);
 
     return 0;
