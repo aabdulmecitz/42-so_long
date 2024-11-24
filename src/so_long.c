14446cf8e7cb37dc25eab8904fe826a132c92e15
aabdulmecitz <ahmetabdulmecid@gmail.com>
2024-11-24 21:23:42 +0300

    commit

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: aabdulmecitz <aabdulmecitz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 03:00:21 by aabdulmecit       #+#    #+#             */
-/*   Updated: 2024/11/24 21:16:37 by aabdulmecit      ###   ########.fr       */
+/*   Updated: 2024/11/24 21:23:37 by aabdulmecit      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,12 +41,12 @@ int	main(int argc, const char *argv[])
     ft_check_command_line_args(argc, argv, game);
 	ft_init_map(game, (char *)argv[1]);
 	ft_init_game(game);
-	printf(CYAN"***here***\n"RESET);
 	ft_check_map(game);
+	printf(CYAN"***here***\n"RESET);
 
     // mlx_hook(game->win_ptr, DestroyNotify, 0, ft_close_game, &game);
 	// mlx_key_hook(game->win_ptr, ft_key, &game);
-	mlx_loop(game->mlx_ptr);
+	// mlx_loop(game->mlx_ptr);
 	return (0);
 }
 
