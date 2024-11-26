eb68cb7cb6fe98af4b1cfa4e1edfb4b07534337a
aabdulmecitz <ahmetabdulmecid@gmail.com>
2024-11-27 02:58:10 +0300

    commit

--- src/ft_render_frame.c
+++ src/ft_render_frame.c
@@ -6,11 +6,13 @@
 /*   By: aabdulmecitz <aabdulmecitz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 05:38:38 by aabdulmecit       #+#    #+#             */
-/*   Updated: 2024/11/27 02:54:21 by aabdulmecit      ###   ########.fr       */
+/*   Updated: 2024/11/27 02:58:06 by aabdulmecit      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+void    ft_paint_texture(t_game *game, int x, int y);
 
 int ft_render_frame(t_game *game)
 {
@@ -22,21 +24,7 @@ int ft_render_frame(t_game *game)
         x = 0;
         while (x < game->map.columns)
         {
-            if (game->map.full[y][x] == WALL)
-                mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->wall.xpm_ptr, x * IMG_WIDTH, y * IMG_HEIGHT);
-            else if (game->map.full[y][x] == FLOOR)
-                mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->floor.xpm_ptr, x * IMG_WIDTH, y * IMG_HEIGHT);
-            else if (game->map.full[y][x] == PLAYER)
-                mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->player_front.xpm_ptr, x * IMG_WIDTH, y * IMG_HEIGHT);    
-            else if (game->map.full[y][x] == COINS)
-                mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->coins.xpm_ptr, x * IMG_WIDTH, y * IMG_HEIGHT);
-            else if (game->map.full[y][x] == MAP_EXIT)
-            {
-                if (game->map.coins == 0)
-                    mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->open_exit.xpm_ptr, x * IMG_WIDTH, y * IMG_HEIGHT);
-                else
-                    mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->exit_closed.xpm_ptr, x * IMG_WIDTH, y * IMG_HEIGHT);
-            }
+            ft_paint_texture(game, x, y);
             x++;
         }
         y++;
@@ -44,3 +32,21 @@ int ft_render_frame(t_game *game)
     return (0);
 }
 
+void    ft_paint_texture(t_game *game, int x, int y)
+{
+    if (game->map.full[y][x] == WALL)
+        mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->wall.xpm_ptr, x * IMG_WIDTH, y * IMG_HEIGHT);
+    else if (game->map.full[y][x] == FLOOR)
+        mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->floor.xpm_ptr, x * IMG_WIDTH, y * IMG_HEIGHT);
+    else if (game->map.full[y][x] == PLAYER)
+        mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->player_front.xpm_ptr, x * IMG_WIDTH, y * IMG_HEIGHT);    
+    else if (game->map.full[y][x] == COINS)
+        mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->coins.xpm_ptr, x * IMG_WIDTH, y * IMG_HEIGHT);
+    else if (game->map.full[y][x] == MAP_EXIT)
+    {
+        if (game->map.coins == 0)
+            mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->open_exit.xpm_ptr, x * IMG_WIDTH, y * IMG_HEIGHT);
+        else
+            mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->exit_closed.xpm_ptr, x * IMG_WIDTH, y * IMG_HEIGHT);
+    }
+}
