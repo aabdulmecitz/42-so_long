f4c6cab1dc584af0ba24921cd88dcd28dbb5f22c
aabdulmecitz <ahmetabdulmecid@gmail.com>
2024-11-27 04:53:50 +0300

    commit

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: aabdulmecitz <aabdulmecitz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 03:00:21 by aabdulmecit       #+#    #+#             */
-/*   Updated: 2024/11/27 00:13:20 by aabdulmecit      ###   ########.fr       */
+/*   Updated: 2024/11/27 02:58:33 by aabdulmecit      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,6 @@ int	main(int argc, const char *argv[])
     game = malloc(sizeof(t_game));
 	if (!game)
 		ft_error_msg("Memory allocation failed for game", game);
-
     ft_check_command_line_args(argc, argv, game);
 	ft_init_map(game, (char *)argv[1]);
 	ft_init_game(game);
