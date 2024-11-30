af8dda15677247ab15f86ab77026faa522baead3
aabdulmecitz <ahmetabdulmecid@gmail.com>
2024-11-30 17:13:34 +0300

    commit

---
+++ bonus/so_long_bonus.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long_bonus.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aabdulmecitz <aabdulmecitz@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/02 03:00:21 by aabdulmecit       #+#    #+#             */
+/*   Updated: 2024/11/30 17:11:48 by aabdulmecit      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long_bonus.h"
+#include <stdio.h>
+
+int	ft_close_game(t_game *game)
+{
+	ft_free_all_allocated_memory(game);
+	
+	ft_printf(GREEN"Game closed successfully.\n"RESET);
+	exit(0);
+	return (0);
+}
+
+void ft_print_map_full(t_game *game)
+{
+    int i;
+
+    for (i = 0; i < game->map.rows; i++)
+    {
+        ft_printf("%s|size: %d\n", game->map.full[i], (int)ft_strlen(game->map.full[i]));
+    }
+}
+
+int	main(int argc, const char *argv[])
+{
+	t_game	*game;
+
+    game = malloc(sizeof(t_game));
+	if (!game)
+		ft_error_msg("Memory allocation failed for game", game);
+    ft_check_command_line_args(argc, argv, game);
+	ft_init_map(game, (char *)argv[1]);
+	ft_init_game(game);
+	ft_check_map(game);
+	ft_printf("player's x = %d, player's y = %d\n", game->map.player.x, game->map.player.y);
+	mlx_loop_hook(game->mlx_ptr, ft_render_frame, game);
+	ft_handle_buttons(game);
+	ft_printf(CYAN"***here***\n"RESET);
+	mlx_loop(game->mlx_ptr);
+	return (0);
+}
+
+
+
---
+++ bonus/so_long_bonus.h
@@ -0,0 +1,162 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aabdulmecitz <aabdulmecitz@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 15:16:28 by aozkaya           #+#    #+#             */
+/*   Updated: 2024/11/30 17:12:10 by aabdulmecit      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_BONUS_H
+# define SO_LONG_BONUS_H
+
+#include "../lib/minilibx-linux/mlx.h"
+#include "../lib/libft/libft.h"
+# include <unistd.h>
+# include <fcntl.h>
+# include <X11/X.h>
+# include <X11/keysym.h>
+# include <stdio.h>
+
+# define IMG_HEIGHT			32
+# define IMG_WIDTH			32
+
+# define ANIM_SPEED			1000
+
+# define WIN_H				1080
+# define WIN_W				1920
+
+# define WALL				'1'
+# define FLOOR 				'0'
+# define COINS  			'C'
+# define PLAYER				'P'
+# define MAP_EXIT 		 	'E'
+
+# define KEY_W				119
+# define KEY_A				97
+# define KEY_S				115
+# define KEY_D				100
+
+# define KEY_UP  			65362
+# define KEY_LEFT  			65361
+# define KEY_RIGHT 			65363
+# define KEY_DOWN  			65364
+
+# define KEY_Q				113
+# define KEY_ESC  			65307
+
+// # define FRONT				1
+// # define LEFT				2
+// # define RIGHT				3
+// # define BACK				4
+
+# define WALL_XPM			"assets/sprites/dungeonwall.xpm"
+# define FLOOR_XPM			"assets/sprites/floor.xpm"
+# define COINS_XPM			"assets/sprites/coin/coin1.xpm"
+# define PLAYER_FRONT_XPM	"assets/sprites/player/front.xpm"
+# define PLAYER_LEFT_XPM	"assets/sprites/player/left.xpm"
+# define PLAYER_RIGHT_XPM	"assets/sprites/player/right.xpm"
+# define PLAYER_BACK_XPM	"assets/sprites/player/back.xpm"
+# define OPEN_EXIT_XPM		"assets/sprites/opened_door.xpm"
+# define EXIT_CLOSED_XPM	"assets/sprites/dungeon_door.xpm"
+
+# define GREEN				"\033[0;32m"
+# define RED 				"\033[1;31m"
+# define GREY 				"\033[0;90m"
+# define CYAN				"\033[1;96m"
+# define RESET 				"\033[0m"
+
+typedef enum e_bool
+{
+	false,
+	true
+}	t_bool;
+
+typedef struct s_position
+{
+	int	x;
+	int	y;
+}	t_positon;
+
+typedef enum e_direction
+{
+    FRONT,
+    BACK,
+    LEFT,
+    RIGHT
+} t_direction;
+
+typedef struct s_image
+{
+	void	*xpm_ptr;
+	int		x;
+	int		y;
+}	t_image;
+
+typedef struct s_map
+{
+	char		**full;
+	int			rows;
+	int			columns;
+	int			coins;
+	int			exit;
+	int			players;
+	t_positon	player;
+}	t_map;
+
+typedef struct s_game
+{
+	void		*mlx_ptr;
+	void		*win_ptr;
+	int			movements;
+	t_direction	player_direction;
+	t_map		map;
+	t_bool		map_alloc;
+	t_image		undefined_image;
+	t_image		wall;
+	t_image		floor;
+	t_image		coins;
+	t_image		open_exit;
+	t_image		exit_closed;
+	t_image		player_front;
+	t_image		player_left;
+	t_image		player_right;
+	t_image		player_back;
+}	t_game;
+
+
+// typedef struct s_anim
+// {
+// 	unsigned int current_frame;
+// 	unsigned int num_of_frames;
+// 	unsigned int speed
+// 	t_image **image_list;
+	
+// } t_anim;
+
+void    ft_check_command_line_args(int argc, char const *argv[], t_game *game);
+void    ft_check_map(t_game *game);
+void    ft_check_for_empty_line(char *map, t_game *game);
+void    ft_error_msg(char *msg, t_game *game);
+void	ft_init_game(t_game *game);
+void	ft_init_map(t_game *game, char *argv);
+void	ft_free_all_allocated_memory(t_game *game);
+void	ft_free_map(t_game *game);
+void	ft_free_just_map(t_map *map);
+void    check_as_a_hero(t_game *game);
+int		key_hook(int keycode, t_game *game);
+int 	ft_check_all_collectables(t_map *map, t_game *game);
+void    ft_handle_buttons(t_game *game);
+int ft_destroy_window(t_game *game);
+void    ft_congrats_message();
+int	ft_render_frame(t_game *game);
+int ft_update_frame(t_game *game);
+
+void ft_print_map_full(t_game *game);
+
+
+
+#endif
