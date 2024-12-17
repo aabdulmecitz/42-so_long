406327bfabdcb041797de3cda56fdea53e1033ae
aabdulmecitz <ahmetabdulmecid@gmail.com>
2024-12-17 03:55:41 +0300

    commit

--- bonus/ft_render_frame.c
+++ bonus/ft_render_frame.c
@@ -6,7 +6,7 @@
 /*   By: aabdulmecitz <aabdulmecitz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 05:38:38 by aabdulmecit       #+#    #+#             */
-/*   Updated: 2024/12/17 03:32:01 by aabdulmecit      ###   ########.fr       */
+/*   Updated: 2024/12/17 03:55:36 by aabdulmecit      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,24 +42,30 @@ int ft_render_frame(t_game *game)
 
 t_image *get_current_coin_frame(t_game *game)
 {
-    static time_t last_time = 0;
+    static clock_t last_time = 0;
     static t_image *current_frame = NULL;
-    time_t current_time = time(NULL);
+    clock_t current_time;
+    double elapsed_time;
+
+    current_time = clock();
+    elapsed_time = (double)(current_time - last_time) / CLOCKS_PER_SEC * 1500.0;
 
     if (current_frame == NULL)
-        current_frame = game->coin_frames; // Animasyon başlangıç çerçevesi
+        current_frame = game->coins;
 
-    if (difftime(current_time, last_time) * 1000 >= ANIMATION_DELAY)
+    if (elapsed_time >= delay)
     {
         current_frame = current_frame->next;
-        if (current_frame == NULL) // Döngüsel animasyon
-            current_frame = game->coin_frames;
+        if (current_frame == NULL)
+            current_frame = game->coins;
         last_time = current_time;
     }
-
     return current_frame;
 }
-
+void ft_paint_coin(t_game *game, int x, int y) {
+    t_image *current_coin_frame = get_current_coin_frame(game);
+    mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, current_coin_frame->xpm_ptr, x * IMG_WIDTH, y * IMG_HEIGHT);
+}
 
 void    ft_paint_texture(t_game *game, int x, int y)
 {
@@ -68,10 +74,7 @@ void    ft_paint_texture(t_game *game, int x, int y)
     else if (game->map.full[y][x] == FLOOR)
         mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->floor->xpm_ptr, x * IMG_WIDTH, y * IMG_HEIGHT);
     else if (game->map.full[y][x] == COINS)
-    {
-        t_image *current_coin_frame = get_current_coin_frame(game); // Animasyon çerçevesini alın
-        mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, current_coin_frame->xpm_ptr, x * IMG_WIDTH, y * IMG_HEIGHT);
-    }
+        ft_paint_coin(game, x, y);
     else if (game->map.full[y][x] == WANDER_ENEMY)
         mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->enemy_x->xpm_ptr, x * IMG_WIDTH, y * IMG_HEIGHT);
     else if (game->map.full[y][x] == STAT_ENEMY)
