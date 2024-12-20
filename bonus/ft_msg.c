6d8a4063a4906b0290a6735f64d15fe49370a1fa
aabdulmecitz <ahmetabdulmecid@gmail.com>
2024-12-21 02:47:42 +0300

    commit

--- bonus/ft_msg.c
+++ bonus/ft_msg.c
@@ -6,7 +6,7 @@
 /*   By: aabdulmecitz <aabdulmecitz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 03:50:53 by aabdulmecit       #+#    #+#             */
-/*   Updated: 2024/12/21 00:54:07 by aabdulmecit      ###   ########.fr       */
+/*   Updated: 2024/12/21 02:46:02 by aabdulmecit      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,7 @@ void	ft_error_msg(char *msg, t_game *game)
 	{
 		if (game->map_alloc)
 			ft_free_map(game);
-		if (game->wall->xpm_ptr)
-			free(game->wall->xpm_ptr);
+		ft_destroy_window(game);
 		free(game);
 		game = NULL;
 	}
