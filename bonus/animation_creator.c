bf0b97e8cf7887e1f84eda94d7dbb0648f9b6c48
aabdulmecitz <ahmetabdulmecid@gmail.com>
2024-12-21 00:34:56 +0300

    commit

--- bonus/animation_creator.c
+++ bonus/animation_creator.c
@@ -6,75 +6,78 @@
 /*   By: aabdulmecitz <aabdulmecitz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 23:35:52 by aabdulmecit       #+#    #+#             */
-/*   Updated: 2024/12/17 05:20:46 by aabdulmecit      ###   ########.fr       */
+/*   Updated: 2024/12/21 00:34:43 by aabdulmecit      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long_bonus.h"
 
-void    init_animation(t_game *game, t_image **image, ...)
-{
-    va_list args;
-    t_image *new_image;
-    const char *current_path;
+void    image_creator(t_game *game, t_image *new_image, const char *current_path);
 
-    va_start(args, image);
-    current_path = va_arg(args, const char *);
-    while (current_path != NULL)
-    {
+void	init_animation(t_game *game, t_image **image, ...)
+{
+	va_list		args;
+	t_image		*new_image;
+	const char	*current_path;
+	t_image		*temp;
+
+	va_start(args, image);
+	current_path = va_arg(args, const char *);
+	while (current_path != NULL)
+	{
         new_image = malloc(sizeof(t_image));
-        if (!new_image)
-            ft_error_msg("Memory allocation failed for image node.", game);
-        ft_load_sprite(new_image, game->mlx_ptr, (char *)current_path, game);
-        new_image->next = NULL;
-        if (*image != NULL)
-        {
-            t_image *temp = *image;
-            while (temp->next != NULL)
-                temp = temp->next;
-            temp->next = new_image;
-        }
-        else
-            *image = new_image;
-        current_path = va_arg(args, const char *);
-    }
-    va_end(args);
+        image_creator(game, new_image, current_path);
+		if (*image != NULL)
+		{
+			temp = *image;
+			while (temp->next != NULL)
+				temp = temp->next;
+			temp->next = new_image;
+		}
+		else
+			*image = new_image;
+		current_path = va_arg(args, const char *);
+	}
+	va_end(args);
 }
 
-void    init_all_of_animations(t_game *game)
+void	init_all_of_animations(t_game *game)
 {
-    init_animation(game, &game->coins, 
-        "assets/sprites/coin/coin2.xpm",
-        "assets/sprites/coin/coin3.xpm",
-        "assets/sprites/coin/coin4.xpm", NULL);
+	init_animation(game, &game->coins, "assets/sprites/coin/coin2.xpm",
+			"assets/sprites/coin/coin3.xpm", "assets/sprites/coin/coin4.xpm",
+			NULL);
 }
 
-
-
-void    run_animation(t_game *game, t_image *image_list)
+void	run_animation(t_game *game, t_image *image_list)
 {
-    static time_t last_time = 0;
-    static t_image *current_image = NULL;
-    time_t current_time;
+	static time_t	last_time;
+	static t_image	*current_image;
+	time_t			current_time;
 
-    current_time = time(NULL);
-
-    if (difftime(current_time, last_time) * 1000 >= delay)
-    {
-        if (current_image == NULL)
-            current_image = image_list;
-        if (current_image && current_image->xpm_ptr)
-        {
-            mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, 
-                current_image->xpm_ptr, current_image->x, current_image->y);
-        }
-        current_image = current_image->next;
-        if (current_image == NULL)
-            current_image = image_list;
-        last_time = current_time;
-        printf("coin ptr: %p\n", current_image->xpm_ptr);
-    }
-
+	last_time = 0;
+	current_image = NULL;
+	current_time = time(NULL);
+	if (difftime(current_time, last_time) * 1000 >= delay)
+	{
+		if (current_image == NULL)
+			current_image = image_list;
+		if (current_image && current_image->xpm_ptr)
+		{
+			mlx_put_image_to_window(game->mlx_ptr, game->win_ptr,
+					current_image->xpm_ptr, current_image->x, current_image->y);
+		}
+		current_image = current_image->next;
+		if (current_image == NULL)
+			current_image = image_list;
+		last_time = current_time;
+		printf("coin ptr: %p\n", current_image->xpm_ptr);
+	}
 }
 
-
+void    image_creator(t_game *game, t_image *new_image, const char *current_path)
+{
+    if (!new_image)
+        ft_error_msg("Memory allocation failed for image node.", game);
+    ft_load_sprite(new_image, game->mlx_ptr, (char *)current_path, game);
+	new_image->next = NULL;
+}
--- bonus/ft_enemy.c
+++ bonus/ft_enemy.c
@@ -6,47 +6,50 @@
 /*   By: aabdulmecitz <aabdulmecitz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 17:59:37 by aabdulmecit       #+#    #+#             */
-/*   Updated: 2024/12/15 22:09:15 by aabdulmecit      ###   ########.fr       */
+/*   Updated: 2024/12/21 00:26:48 by aabdulmecit      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long_bonus.h"
 
+void	ft_init_enemy_value(t_enemy *new_enemy, t_game *game, int x, int y);
 
-void ft_init_enemy(t_game *game)
+void	ft_init_enemy(t_game *game)
 {
-    int x;
-    int y;
-    t_enemy *new_enemy;
+	int		x;
+	int		y;
+	t_enemy	*new_enemy;
 
-    game->enemy = NULL;
-    y = 0;
-
-    while (y < game->map.rows)
-    {
-        x = 0;
-        while (x < game->map.columns)
-        {
-            if (game->enemy_x_num == 0)
-                return;
-            if (game->map.full[y][x] == WANDER_ENEMY)
-            {
-                new_enemy = (t_enemy *)malloc(sizeof(t_enemy));
-                if (!new_enemy)
-                    return;
-                new_enemy->x = x;
-                new_enemy->y = y;
-                new_enemy->dir = rand() % 4;
-                new_enemy->last_move_time = time(NULL);
-                new_enemy->next = game->enemy;
-                game->enemy = new_enemy;
-                game->enemy_x_num--;
-            }
-            x++;
-        }
-        y++;
-    }
+	game->enemy = NULL;
+	y = 0;
+	while (y < game->map.rows)
+	{
+		x = 0;
+		while (x < game->map.columns)
+		{
+			if (game->enemy_x_num == 0)
+				return ;
+			if (game->map.full[y][x] == WANDER_ENEMY)
+			{
+				new_enemy = (t_enemy *)malloc(sizeof(t_enemy));
+				if (!new_enemy)
+					return ;
+				ft_init_enemy_value(new_enemy, game, x, y);
+			}
+			x++;
+		}
+		y++;
+	}
 }
 
-
-// IF ENEMY SEE THE PLAYER YOU WILL LOSE 
+void	ft_init_enemy_value(t_enemy *new_enemy, t_game *game, int x, int y)
+{
+	new_enemy->x = x;
+	new_enemy->y = y;
+	new_enemy->dir = rand() % 4;
+	new_enemy->last_move_time = time(NULL);
+	new_enemy->next = game->enemy;
+	game->enemy = new_enemy;
+	game->enemy_x_num--;
+}
+// IF ENEMY SEE THE PLAYER YOU WILL LOSE
