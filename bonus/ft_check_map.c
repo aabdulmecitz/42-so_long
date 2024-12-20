f56946d96090929f150b279fd669c235eeffd5a6
aabdulmecitz <ahmetabdulmecid@gmail.com>
2024-12-20 05:06:47 +0300

    norminette v1

--- bonus/ft_check_map.c
+++ bonus/ft_check_map.c
@@ -6,100 +6,107 @@
 /*   By: aabdulmecitz <aabdulmecitz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 19:32:02 by aabdulmecit       #+#    #+#             */
-/*   Updated: 2024/12/16 00:22:29 by aabdulmecit      ###   ########.fr       */
+/*   Updated: 2024/12/20 05:05:39 by aabdulmecit      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long_bonus.h"
 
-void    ft_check_rectanglular(t_game *game);
-void    ft_check_elements(t_game *game);
-void    ft_search_elements(t_game *game);
-void    ft_count_elements(t_game *game, int x, int y);
+void	ft_check_rectanglular(t_game *game);
+void	ft_check_elements(t_game *game);
+void	ft_search_elements(t_game *game);
+void	ft_count_elements(t_game *game, int x, int y);
 
-void    ft_check_map(t_game *game)
-{ 
-    ft_check_rectanglular(game);
-    ft_check_elements(game);
-    check_as_a_hero(game);
+void	ft_check_map(t_game *game)
+{
+	ft_check_rectanglular(game);
+	ft_check_elements(game);
+	check_as_a_hero(game);
 }
 
-void    ft_check_rectanglular(t_game *game)
+void	ft_check_rectanglular(t_game *game)
 {
-    int i;
-    int first_row_len;
-    int current_row_len;
+	int	i;
+	int	first_row_len;
+	int	current_row_len;
 
-    if (game->map.rows == 0)
-        return;
-    first_row_len = (int)ft_strlen(game->map.full[0]);
-    i = 1;
-    while (i < game->map.rows)
-    {
-        current_row_len = (int)ft_strlen(game->map.full[i]);
-        if (current_row_len != first_row_len)
-        {
-            ft_printf(RED "%s , current row len: %d , first row len: %d\n" RESET, game->map.full[i], current_row_len, first_row_len);
-            ft_error_msg("Map is not rectangular!", game);
-            return;
-        }
-        i++;
-    }
-    game->map.columns++;
-    ft_printf(GREEN "Map is rectangular!\n" RESET);
+	if (game->map.rows == 0)
+		return ;
+	first_row_len = (int)ft_strlen(game->map.full[0]);
+	i = 1;
+	while (i < game->map.rows)
+	{
+		current_row_len = (int)ft_strlen(game->map.full[i]);
+		if (current_row_len != first_row_len)
+		{
+			ft_printf(RED "%s , current row len: %d , first row len: \
+				%d\n" RESET, game->map.full[i], current_row_len, \
+				first_row_len);
+			ft_error_msg("Map is not rectangular!", game);
+			return ;
+		}
+		i++;
+	}
+	game->map.columns++;
+	ft_printf(GREEN "Map is rectangular!\n" RESET);
 }
 
-void    ft_check_elements(t_game *game)
+void	ft_check_elements(t_game *game)
 {
-    ft_search_elements(game);
-    if (game->map.players != 1)
-        ft_error_msg("Map must contain exactly one starting position ('P')", game);
-    if (game->map.exit != 1)
-        ft_error_msg("Map must contain exactly one exit ('E')", game);
-    if (game->map.coins < 1)
-        ft_error_msg("Map must contain at least one collectible ('C')", game);
+	ft_search_elements(game);
+	if (game->map.players != 1)
+		ft_error_msg("Map must contain exactly one starting position ('P')", \
+			ame);
+	if (game->map.exit != 1)
+		ft_error_msg("Map must contain exactly one exit ('E')", game);
+	if (game->map.coins < 1)
+		ft_error_msg("Map must contain at least one collectible ('C')", game);
 }
 
-void    ft_search_elements(t_game *game)
+void	ft_search_elements(t_game *game)
 {
-    int y;
-    int x;
+	int	y;
+	int	x;
 
-    y = 0;
-    while (y < game->map.rows)
-    {
-		ft_printf(CYAN"|%s| size: %d|\n" RESET, game->map.full[y], (int)ft_strlen(game->map.full[y]));
-        x = 0;
-        while (x < game->map.columns)
-        {
-            ft_count_elements(game, x, y);
-            x++;
-        }
-        y++;
-    }
+	y = 0;
+	while (y < game->map.rows)
+	{
+		ft_printf(CYAN "|%s| size: %d|\n" RESET, game->map.full[y], \
+				(int)ft_strlen(game->map.full[y]));
+		x = 0;
+		while (x < game->map.columns)
+		{
+			ft_count_elements(game, x, y);
+			x++;
+		}
+		y++;
+	}
 }
 
-void    ft_count_elements(t_game *game, int x, int y)
+void	ft_count_elements(t_game *game, int x, int y)
 {
-    if ((y == 0 || y == game->map.rows - 1 || x == 0 || x == game->map.columns - 1) && game->map.full[y][x] != WALL)
-        ft_error_msg("Invalid Map.", game);
-    if (game->map.full[y][x] == PLAYER)
-    {
-        game->map.players++;
-        game->map.player.y = y;
-        game->map.player.x = x;
-    }
-    else if (game->map.full[y][x] == MAP_EXIT)
-        game->map.exit++;
-    else if (game->map.full[y][x] == COINS)
-        game->map.coins++;
-    else if (game->map.full[y][x] == STAT_ENEMY)
-        game->enemy_k_num++;
-    else if (game->map.full[y][x] == WANDER_ENEMY)
-        game->enemy_x_num++;
-    else if (game->map.full[y][x] != WALL && game->map.full[y][x] != FLOOR &&
-        game->map.full[y][x] != PLAYER && game->map.full[y][x] != MAP_EXIT && 
-        game->map.full[y][x] != COINS && game->map.full[y][x] != WANDER_ENEMY &&
-        game->map.full[y][x] != STAT_ENEMY)
-        ft_error_msg("Invalid character in map", game);
+	if ((y == 0 || y == game->map.rows - 1 || x == 0 || \
+x == game->map.columns - 1) && game->map.full[y][x] != WALL)
+		ft_error_msg("Invalid Map.", game);
+	if (game->map.full[y][x] == PLAYER)
+	{
+		game->map.players++;
+		game->map.player.y = y;
+		game->map.player.x = x;
+	}
+	else if (game->map.full[y][x] == MAP_EXIT)
+		game->map.exit++;
+	else if (game->map.full[y][x] == COINS)
+		game->map.coins++;
+	else if (game->map.full[y][x] == STAT_ENEMY)
+		game->enemy_k_num++;
+	else if (game->map.full[y][x] == WANDER_ENEMY)
+		game->enemy_x_num++;
+	else if (game->map.full[y][x] != WALL && game->map.full[y][x] != FLOOR &&
+				game->map.full[y][x] != PLAYER
+					&& game->map.full[y][x] != MAP_EXIT &&
+				game->map.full[y][x] != COINS
+					&& game->map.full[y][x] != WANDER_ENEMY &&
+				game->map.full[y][x] != STAT_ENEMY)
+		ft_error_msg("Invalid character in map", game);
 }
--- bonus/ft_enemy_movement.c
+++ bonus/ft_enemy_movement.c
@@ -6,80 +6,84 @@
 /*   By: aabdulmecitz <aabdulmecitz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 02:42:34 by aozkaya           #+#    #+#             */
-/*   Updated: 2024/12/16 00:49:51 by aabdulmecit      ###   ########.fr       */
+/*   Updated: 2024/12/20 05:04:40 by aabdulmecit      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long_bonus.h"
 
-int is_valid_position(t_game *game, int x, int y)
+void	move_enemy(t_game *game, t_enemy *enemy int new_x, int new_y);
+
+int	is_valid_position(t_game *game, int x, int y)
 {
-    if (x < 0 || x >= game->map.columns || y < 0 || y >= game->map.rows)
-        return 0;
-    if (game->map.full[y][x] == WALL || game->map.full[y][x] == STAT_ENEMY || 
-        game->map.full[y][x] == WANDER_ENEMY || game->map.full[y][x] == COINS ||
-        game->map.full[y][x] == MAP_EXIT)
-        return 0;
-    return 1;
+	if (x < 0 || x >= game->map.columns || y < 0 || y >= game->map.rows)
+		return (0);
+	if (game->map.full[y][x] == WALL || game->map.full[y][x] == STAT_ENEMY ||
+		game->map.full[y][x] == WANDER_ENEMY || game->map.full[y][x] == COINS ||
+		game->map.full[y][x] == MAP_EXIT)
+		return (0);
+	return (1);
 }
 
-void ft_enemy_movement(t_game *game, t_enemy *enemy)
+void	ft_enemy_movement(t_game *game, t_enemy *enemy)
 {
-    int new_x, new_y;
-    time_t current_time;
+	time_t	current_time;
+	int		new_x;
+	int		new_y;
 
-    current_time = time(NULL);
-    if (difftime(current_time, enemy->last_move_time) < 0.001) {
-        return;
-    }
-    new_x = enemy->x;
-    new_y = enemy->y;
-    if (enemy->dir == BACK)
-        new_y--;
-    else if (enemy->dir == FRONT)
-        new_y++;
-    else if (enemy->dir == RIGHT)
-        new_x++;
-    else if (enemy->dir == LEFT)
-        new_x--;
-    // printf("Enemy moving from (%d, %d) to (%d, %d)\n", enemy->x, enemy->y, new_x, new_y);
-    if (is_valid_position(game, new_x, new_y))
-    {
-        if (game->map.full[new_y][new_x] == PLAYER)
-        {
-            ft_failed_msg();
-            ft_destroy_window(game);
-        }
-        game->map.full[enemy->y][enemy->x] = FLOOR;
-        enemy->x = new_x;
-        enemy->y = new_y;
-        game->map.full[new_y][new_x] = WANDER_ENEMY;
-    }
-    else
-    {
-        enemy->dir = rand() % 4;
-        if (enemy->dir == 0)
-            enemy->dir = BACK;
-        else if (enemy->dir == 1)
-            enemy->dir = FRONT;
-        else if (enemy->dir == 2)
-            enemy->dir = LEFT;
-        else
-            enemy->dir = RIGHT;
-    }
-    enemy->last_move_time = current_time;
+	current_time = time(NULL);
+	if (difftime(current_time, enemy->last_move_time) < 0.001)
+		return ;
+	new_x = enemy->x;
+	new_y = enemy->y;
+	if (enemy->dir == BACK)
+		new_y--;
+	else if (enemy->dir == FRONT)
+		new_y++;
+	else if (enemy->dir == RIGHT)
+		new_x++;
+	else if (enemy->dir == LEFT)
+		new_x--;
+	move_enemy(game, enemy, new_x, new_y);
+	enemy->last_move_time = current_time;
 }
 
-void ft_handle_enemies(t_game *game)
+void	move_enemy(t_game *game, t_enemy *enemy int new_x, int new_y)
 {
-    t_enemy *current;
+	if (is_valid_position(game, new_x, new_y))
+	{
+		if (game->map.full[new_y][new_x] == PLAYER)
+		{
+			ft_failed_msg();
+			ft_destroy_window(game);
+		}
+		game->map.full[enemy->y][enemy->x] = FLOOR;
+		enemy->x = new_x;
+		enemy->y = new_y;
+		game->map.full[new_y][new_x] = WANDER_ENEMY;
+	}
+	else
+	{
+		enemy->dir = rand() % 4;
+		if (enemy->dir == 0)
+			enemy->dir = BACK;
+		else if (enemy->dir == 1)
+			enemy->dir = FRONT;
+		else if (enemy->dir == 2)
+			enemy->dir = LEFT;
+		else
+			enemy->dir = RIGHT;
+	}
+}
 
-    current = game->enemy;
-    while (current)
-    {
-        // printf("Enemy: x: %d, y: %d\n", current->x, current->y);
-        ft_enemy_movement(game, current);
-        current = current->next;
-    }
-    // printf("===========================\n");
+void	ft_handle_enemies(t_game *game)
+{
+	t_enemy	*current;
+
+	current = game->enemy;
+	while (current)
+	{
+		ft_enemy_movement(game, current);
+		current = current->next;
+	}
 }
