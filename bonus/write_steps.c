bd303dbc8aac857a3a6cdac8aaf546068d50f046
Ahmet abdülmecit Özkaya <aozkaya@k1m07s04.42istanbul.com.tr>
2024-12-21 17:35:29 +0300

    commit

--- bonus/write_steps.c
+++ bonus/write_steps.c
@@ -6,7 +6,7 @@
 /*   By: aozkaya <aozkaya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 23:52:57 by aabdulmecit       #+#    #+#             */
-/*   Updated: 2024/12/21 16:33:45 by aozkaya          ###   ########.fr       */
+/*   Updated: 2024/12/21 17:33:52 by aozkaya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,11 @@
 
 void	write_steps(t_game *game, int x, int y)
 {
+	char *movement;
+
+	movement = ft_itoa(game->movements);
 	mlx_string_put(game->mlx_ptr, game->win_ptr, x + 64, y, 0xFFFFFF, \
-			ft_itoa(game->movements));
+			movement);
 	mlx_string_put(game->mlx_ptr, game->win_ptr, x, y, 0xFFFFFF, "Movements: ");
+	free(movement);
 }
