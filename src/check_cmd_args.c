8d727da03620bd5d98bd2f31a4b00b7a421ca0c3
Ahmet abdülmecit Özkaya <aozkaya@k1m15s06.42istanbul.com.tr>
2025-05-31 19:10:53 +0300

    gasdf

--- src/check_cmd_args.c
+++ src/check_cmd_args.c
@@ -17,10 +17,9 @@ void	check_cmd_args(int argc, char const *argv[], t_ctx *ctx)
 	int	map_len;
 
 	if (argc < 2)
-		error("Where is the map bro? Please Enter a map!", ctx);
+		error("Please Enter a map!", ctx);
 	if (argc > 2)
-		error("You entered alot of argument. \
-            You must enter one map file without ctx", ctx);
+		error("You entered alot of argument. ", ctx);
 	map_len = ft_strlen(argv[1]);
 	if (!ft_strnstr(&argv[1][map_len - 4], ".ber", 4))
 		error("Map file is not valid. You can enter just .ber file.", \
